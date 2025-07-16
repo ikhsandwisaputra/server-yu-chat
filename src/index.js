2262031bbc26630f7accf2598cf4f47b575a82d1
ikhsandwisaputra <ikhsandwisaputraaaa@gmail.com>
2025-07-16 18:16:21 +0700

    change server

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const onlineUsers = new Map();
 const io = new Server(server, {
   cors: {
-   origin: "http://localhost:5173",// Ganti dengan URL frontend React kamu
+   origin: "ikhsandwisaputra.github.io/yu-chat/",// Ganti dengan URL frontend React kamu
     methods: ["GET", "POST"],
   },
 });
